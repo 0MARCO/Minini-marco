63b8e9aade4c3cd6293a941d9412877785c1f124
0MARCO <75375475+0MARCO@users.noreply.github.com>
2021-02-24 23:56:22 +0100

    Add files via upload

---
+++ Minini_Marco_3_E_riepilogo.py
@@ -0,0 +1,58 @@
+#prima parte nome e cognome
+#nome= str(input("Nome dell'utente: "))
+#cognome= str(input("Cognome dell'utente: "))
+
+#print: nome
+#print: (nome, cognome)
+
+#stringa = str(input("inserisci nome e cognome: "))
+
+#i = 0
+#while i < len(stringa):
+    #print (stringa[i])
+    #i = i + 1
+
+#print(i)
+
+#seconda parte
+#numero1= str(input('inserisci primo numero: '))
+#numero2= str(input('inserisci secondo numero: '))
+
+#def largest(num1, num2):
+    #if (num1 < num2 ):
+       #largest_num = num1
+    #elif (num2 < num1):
+       #largest_num = num2
+   # print("il numero più grande dei 2  è: ", largest_num)
+
+#def smallest(num1, num2):
+    #if (num1 < num2 ):
+       #smallest_num = num1
+    #elif (num2 < num1):
+       #smallest_num = num2
+    #print("il numero più piccolo dei 2  è: ", smallest_num)
+
+#smallest(numero1, numero2)
+#largest(numero1, numero2)
+
+#terza parte
+#colore= str(input("qual è il tuo colore preferito?: "))
+#if rosso = bello
+#else altricolori = non, mi, piace
+
+#quarta parte
+#quinta parte
+nome= str(input("inserisci nome: "))
+i = 0
+while i < len(nome):
+    print (nome[i])
+    i = i + 1
+
+print(i)
+#sesta parte
+#settima parte
+#ottava parte
+#nona parte
+
+
+
