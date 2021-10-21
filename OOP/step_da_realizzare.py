59f8da11b9ab3456bcde3a0f19c503e4ed940fbc
0MARCO <75375475+0MARCO@users.noreply.github.com>
2021-10-21 10:33:19 +0200

    Create step_da_realizzare.py

---
+++ OOP/step_da_realizzare.py
@@ -0,0 +1,15 @@
+class calcComb()
+
+   def __init__(self, stringa):
+
+        self.__N = len(stringa)
+        self.__stringa = stringa
+        self.__listStringa = list(stringa)
+
+    def get_stringa(self):
+        return self.__stringa
+
+    def get_listStringa(self):
+        return self.__listStringa
+
+P = n!/(n-n)!
