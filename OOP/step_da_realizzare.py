f6823ef6c389503cf48d9cde5e249fa3e9898a86
0MARCO <75375475+0MARCO@users.noreply.github.com>
2021-11-11 20:37:26 +0100

    Update step_da_realizzare.py

--- OOP/step_da_realizzare.py
+++ OOP/step_da_realizzare.py
@@ -1,6 +1,6 @@
-class calcComb()
+class calcComb():
 
-   def __init__(self, stringa):
+    def __init__(self, stringa):
 
         self.__N = len(stringa)
         self.__stringa = stringa
@@ -12,4 +12,70 @@ class calcComb()
     def get_listStringa(self):
         return self.__listStringa
 
-P = n!/(n-n)!
+    def setStringa(self):
+        '''
+        modificare questo metodo in modo da verificare la coerenza delle variabili di
+        istanza presenti
+        '''
+        return 0
+
+    def charRipetuti(self):
+        ogni chiave deve essere ogni carattere della stringa e edeve associare il valore alla chiave quante volte si ripete quel carattere
+      stringa = {
+        questo metodo deve creare un dictionary all'interno del quale la chiave deve essere
+        il singolo carattere, il valore deve essere il numero di ripetizioni di quel carattere
+        
+        esempi di dictionary sono presenti nel file elementi_base/dictionary.py
+        '''
+
+    def confUtil(self):
+        '''
+        verificare se la STRINGA attributo di istanza è presente
+        nel file word.italian.txt 
+        '''
+        pass
+
+    def fattoriale(n):
+        '''
+        implementare una qualunque versione della funzione fattoriale
+        '''
+
+    def coeffBinom(n, k):
+        P = n!/K!(n-K!)!
+        
+
+    # PERMUTAZIONI
+
+    def nPermutSenzaRip(self):
+      
+        return P = n1 
+     
+      
+    def nPermutConRip(self):
+       
+        return n!/a1! x a2! x ak!...
+
+    
+    # DISPOSIZIONI
+
+    def nDispSemplSenzaRip(self):
+      
+        return P = n!/ (n-K)!
+
+    def nDispSemplConRip(self):
+       
+        return P = n^k
+
+    # COMBINAZIONI
+
+    def nCombSemplSenzaRip(self):
+        
+        return P = n!/K!(n-k)!
+
+    def nCombSemplConRip(self):
+       
+        return P = (n + k - 1)/K!(n-1)!
+
+    
+
+
