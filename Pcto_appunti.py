faa4f8fefd94ca08e0e9841181e19d9608823889
0MARCO <75375475+0MARCO@users.noreply.github.com>
2021-12-10 15:51:35 +0100

    Create Pcto_appunti.py

---
+++ Pcto_appunti.py
@@ -0,0 +1,43 @@
+#Primo codice
+print (' Ciao Mondo!') # per convenzione usiamo questo '
+
+#Secondo codice
+print('io\'sono Antonio') #(lo conta come testo se mettiamo \)
+
+#Terzo codice( per creare nuova riga
+print('ciao')
+print('')
+print('Antonio')
+print ('Ciao\nAntonio') #se mettiamo \n fa nuaova riga
+print ('ciao' + 'ciao') #li somma e li scrive insieme
+print ('ciao' , 'ciao') #aggiunge uno spazio
+
+#Quarto codie
+print ('trent\'anni fa è nato il Python\nMa è sempre un gran bel linguaggio')
+
+#Quinto codice
+topolino = 'un topo troppo cresciuto' #niente numeri iniziali nelle variabili
+paperino = 'quando parla non si capisce'
+pippo = 'idolo di tutti i bambini'
+print (topolino) #per le variabili non dobbiamo mettere l'apicetto)
+
+#Sesto codice
+paperino = 4 #sto dicendo che questo è un unmero intero, quindi ci darà come risultato un numero intero
+pipp = 4.5 #numero con la virgola
+minnie = 4. #per lui è sempre un numero con la virgola
+
+#Settimo codice
+sono_un_somaro = True #hanno sempre la lettera maiuscola vero e falso
+sono_un_somaro = False
+paperina = 3_45_675_345_675_346_435_467_845_645_676_879_085 #_ è UN MODO PER SCRIVERE BENE UN NUMERO SENZA CHE PYTHON LO VA A VEDERE
+print (0.000000000001) # lo converte direttamente in notazione scientifica esce (1e-20)
+
+#Ottavo codice
+print (0b11) #0 fa la conversione, se aggingiamo b, bianrio, o ottario, x essagesimale
+2*2 #moltiplicazione
+2/2#divisione
+2-2#sottrazione
+2+2#addizione
+2//2#ci da un numero intero
+5%2 #fa 5 diviso 2 e verra un numero con il resto, la perc ci fa uscire il resto 
+2**5 #potenza
